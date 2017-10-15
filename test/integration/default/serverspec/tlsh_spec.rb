9e93495452e122e3e7fc61013d929d38575f1245
juju4 <juju4@users.noreply.github.com>
2017-10-15 18:34:00 -0400

    tlsh 3.7.3: optional shared library -> option + more serverspec testing

--- test/integration/default/serverspec/tlsh_spec.rb
+++ test/integration/default/serverspec/tlsh_spec.rb
@@ -9,6 +9,10 @@ set :backend, :exec
 #    c.formatter = 'JUnit'
 #end
 
+describe file('/opt/tools/tlsh/lib/libtlsh.a') do
+  it { should be_readable }
+end
+
 describe file('/opt/tools/tlsh/lib/libtlsh.so') do
   it { should be_readable }
 end
@@ -24,10 +28,18 @@ describe command('/opt/tools/tlsh/bin/tlsh_version') do
   its(:stdout) { should match /buckets hash/ }
   its(:exit_status) { should eq 0 }
 end
+
 describe command('/opt/tools/tlsh/Testing/test.sh') do
   its(:stdout) { should match /passed/ }
   its(:exit_status) { should eq 0 }
 end
+
+describe command('/opt/tools/tlsh/Testing/test.sh -xlen') do
+  its(:stdout) { should match /passed/ }
+  its(:stdout) { should match /passed all example data tests/ }
+  its(:exit_status) { should eq 0 }
+end
+
 describe command('/opt/tools/tlsh/Testing/python_test.sh') do
   its(:stdout) { should match /passed/ }
   its(:exit_status) { should eq 0 }
