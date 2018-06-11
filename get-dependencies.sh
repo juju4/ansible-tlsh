012b18beb2818e27995a92e017f70311336217c9
juju4 <juju4@users.noreply.github.com>
2018-06-11 18:35:11 -0400

    missing role dependency

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -9,7 +9,7 @@ else
 rolesdir=$(dirname $0)/..
 fi
 
-#[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
 ## galaxy naming: kitchen fails to transfer symlink folder
 #[ ! -e $rolesdir/juju4.tlsh ] && ln -s ansible-tlsh $rolesdir/juju4.tlsh
 [ ! -e $rolesdir/juju4.tlsh ] && cp -R $rolesdir/ansible-tlsh $rolesdir/juju4.tlsh
